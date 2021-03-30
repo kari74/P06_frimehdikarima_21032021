0ccdcf6a9627c7da356cdf0513df203ae9ed4b31
karima frimehdi <frimehdi.karima@gmail.com>
2021-03-30 14:37:23 +0200

    securité

--- backend/app.js
+++ backend/app.js
@@ -2,16 +2,18 @@ const express = require('express');
 const mongoose = require ('mongoose') 
 const path = require('path');
 const helmet = require('helmet')
-require('dotenv').config() 
+
+ require('dotenv').config() 
+
 
 //importation des routes
 const sauceRoutes = require('./routes/sauces');
 const userRoutes = require('./routes/user');
 
-//mongoose.connect(MONGODB_URL,
-//mongoose.connect(`mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.nhnqu.mongodb.net/${process.env.DB_DATABASE}?retryWrites=true&w=majority`,
 
-mongoose.connect('mongodb+srv://frimehdikarima:mdpsopekocko@cluster0.nhnqu.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',
+mongoose.connect(`mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.nhnqu.mongodb.net/${process.env.DB_DATABASE}?retryWrites=true&w=majority`,
+
+//mongoose.connect('mongodb+srv://frimehdikarima:mdpsopekocko@cluster0.nhnqu.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',
     { useNewUrlParser: true,
     useUnifiedTopology: true })
   .then(() => console.log('Connexion à MongoDB réussie !'))
@@ -31,8 +33,10 @@ app.use((req, res, next) => {
 app.use(express.json());
 app.use('/images', express.static(path.join(__dirname, 'images')));
 
+
+app.use(helmet());
 //routes
 app.use('/api/sauces', sauceRoutes);
 app.use('/api/auth', userRoutes);
-app.use(helmet());
+
 module.exports = app;
